b248581cf7c46f5ff4624c5f691468ad8a271656
Luo <luowc5@163.com>
2022-08-25 21:26:19 +0800

    Add the original layout and style

---
+++ src/component/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Note from "./note";
+import Footer from "./Footer";
+
+function App(){
+    return <div>
+        <Header></Header>
+        <Note></Note>
+        <Footer></Footer>
+    </div>
+}
+
+export default App;
